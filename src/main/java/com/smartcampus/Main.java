b26214be9655ddc42a74af40dc91ab1854dafd5d
Sadana Suresh <sadanasuresh001@gmail.com>
2026-04-21 11:17:42 +0100

    Refactor server URI and resource configuration

--- src/main/java/com/smartcampus/Main.java
+++ src/main/java/com/smartcampus/Main.java
@@ -1,9 +1,9 @@
 package com.smartcampus;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -13,28 +13,32 @@ public class Main {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
+    // this is the base path the API should run on
     public static final String BASE_URI = "http://localhost:8080/api/v1/";
-    private static final String SERVER_URI = "http://localhost:8080/";
 
     public static void main(String[] args) throws IOException {
 
-        ResourceConfig rc = ResourceConfig.forApplicationClass(SmartCampusApplication.class);
+        ResourceConfig rc = new ResourceConfig();
 
-        // register JSON support, resources, exception mappers, and filters here
+        // JSON support
         rc.register(JacksonFeature.class);
 
+        // resources
         rc.register(com.smartcampus.resource.DiscoveryResource.class);
         rc.register(com.smartcampus.resource.RoomResource.class);
         rc.register(com.smartcampus.resource.SensorResource.class);
 
+        // exception mappers
         rc.register(com.smartcampus.exception.RoomNotEmptyExceptionMapper.class);
         rc.register(com.smartcampus.exception.LinkedResourceNotFoundExceptionMapper.class);
         rc.register(com.smartcampus.exception.SensorUnavailableExceptionMapper.class);
         rc.register(com.smartcampus.exception.GlobalExceptionMapper.class);
 
+        // filters
         rc.register(com.smartcampus.filter.ApiLoggingFilter.class);
 
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(SERVER_URI), rc);
+        // mount the server directly on the versioned API base path
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
 
         LOGGER.info("Smart Campus API running at: " + BASE_URI);
         LOGGER.info("Press ENTER to stop the server");
